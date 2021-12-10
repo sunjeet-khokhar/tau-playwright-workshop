2495c0bf1e2b5b71d5290acd91f8fa85c8f5c2b6
Sunjeet Khokhaer <sunjeet81@gmail.com>
2021-12-10 14:26:16 +1300

    comment code that was refactored

--- tests/test_search_sk.py
+++ tests/test_search_sk.py
@@ -1,9 +1,25 @@
 
+
 def test_duckduckgo_search(home_page,results_page,common_methods):
     # Given the DuckDuckGo homepage is diisplayed
     # when the user searches for pickles
     #then the search result results are related to pickles 
     #and the title of the page also reflects pickles 
+    
+    ''' Original code that has been refactored to use POM and PyTest fixtures
+    page.fill("[id='search_form_input_homepage']",'pickles')
+    page.click("[id='search_button_homepage']")
+    assert 'pickles' == page.input_value("#search_form_input")
+    #wait for some result titles to load , being defensive
+    page.locator("[data-testid=result-title-a] >> nth=4").wait_for()
+    page_titles = page.locator("[data-testid=result-title-a]").all_inner_texts()
+    print(len(page_titles))
+    assert len(page_titles) >0 
+    for t in page_titles: 
+        print(t)
+        # weak test !
+        assert('pick' in t.lower())
+    assert('pickle' in page.title().lower())'''
     
     search_term = 'pickles'
     home_page.load()
@@ -19,28 +35,8 @@ def test_duckduckgo_search(home_page,results_page,common_methods):
         print(t)
         # weak test !
         assert('pick' in t.lower())
-    try:
-        assert('pickle1' in common_methods.get_title_of_page().lower())
-    except AssertionError as e:
-        print("TEST FAILED ! Desired text was not found in the page title")
-        print("Detailed error as below --> \n" )
-        print(e)
+    assert('pickle1' in common_methods.get_title_of_page().lower())
         
-            
-    
-    ''' Code that has been refactor as above to use POM and PyTest fixtures
-    page.fill("[id='search_form_input_homepage']",'pickles')
-    page.click("[id='search_button_homepage']")
-    assert 'pickles' == page.input_value("#search_form_input")
-    #wait for some result titles to load , being defensive
-    page.locator("[data-testid=result-title-a] >> nth=4").wait_for()
-    page_titles = page.locator("[data-testid=result-title-a]").all_inner_texts()
-    print(len(page_titles))
-    assert len(page_titles) >0 
-    for t in page_titles: 
-        print(t)
-        # weak test !
-        assert('pick' in t.lower())
-    assert('pickle' in page.title().lower())'''
+
     
 
